feedbdd2d74203e4a6340f6939a3406991c3fd5b
AdityaJindal07 <adityajindal704@gmail.com>
2025-07-28 05:09:34 +0530

    try-1

--- server_gdg.js
+++ server_gdg.js
@@ -297,7 +297,7 @@ app.get("/do-fetch-all", verifyToken, function (req, resp) {
         else {
             for (var i = 0; i < allRecords.length; i++) {
                 if (allRecords[i].deadline) {
-                    allRecords[i].deadline = allRecords[i].deadline + '+05:30';
+                    allRecords[i].deadline = allRecords[i].deadline - '+05:30';
                 }
             }
             resp.send(allRecords);
